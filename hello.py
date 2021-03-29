2c0afa727be063844661f70b28adef2403c37d03
bikoding <80879316+bikoding@users.noreply.github.com>
2021-03-29 13:37:13 +0700

    Add hello.py

---
+++ hello.py
@@ -0,0 +1,9 @@
+menu = "0"
+while menu != "X":
+    print("Pilih 1 Untuk Input Data")
+    print("Pilih 2 Untuk Lihat Data")
+    print("Pilih X Untuk Keluar")
+    menu = input("Pilih Menu: ")
+    print("\n")
+    if menu == 'X':
+        exit
